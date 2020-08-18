ce9dc1bf3decdd8f4fc9711ff2cc0ee802374c2a
Victoria Landron <viclandron@gmail.com>
2020-08-18 17:48:28 -0400

    looped the array so you can continue fighting robots

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,13 +3,17 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+//Game States
+// "WIN" - Player robot has defeated all enemy robots
+// * Fight all enemy robots
+// * Defeat each enemy robot
+// "LOSE" - Player robot's health is zero or less
+
+var fight = function(enemyName) {
     //Alert users that they are starting the round
     window.alert("Welcome to Robot Galdiators!");
 
@@ -72,4 +76,6 @@ var fight = function() {
     }
 };
 
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
