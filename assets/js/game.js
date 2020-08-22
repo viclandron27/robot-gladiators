7a074c775b45efeb5de8de03cd31fcc86daaaa58
Victoria Landron <viclandron@gmail.com>
2020-08-22 11:03:16 -0400

    shop function allows for integers

--- assets/js/game.js
+++ assets/js/game.js
@@ -162,21 +162,20 @@ endGame();
 var shop = function () {
     //ask player what they'd like to do
     var shopOptionPrompt = prompt(
-        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
     );
+    
+    shopOptionPrompt = parseInt(shopOptionPrompt);
 
     //use switch to carry out action
     switch (shopOptionPrompt) {
-        case "REFILL": 
-        case "refill":
+        case 1:
             playerInfo.refillHealth();
             break;
-        case "UPGRADE":
-        case "upgrade":
+        case 2:
             playerInfo.refillAttack();
             break;
-        case "LEAVE":
-        case "leave":
+        case 3:
             alert("Leave the store");
 
             //do nothing, so function will end
